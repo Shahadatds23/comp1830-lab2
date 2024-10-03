2e161f1a43bd01f57d04b7156f457d6d8be99516
Shahadatds23 <126831199+Shahadatds23@users.noreply.github.com>
2024-10-03 16:40:12 +0100

    Update get-utxo.py

--- accounting-models/get-utxo.py
+++ accounting-models/get-utxo.py
@@ -3,10 +3,11 @@ import json
 import requests
 
 # Target address
-address = '<address here>'
+address = '<1Dorian4RoXcnBv9hnQ4Y2C1an6NJ4UrjX>'
 
 resp = requests.get('https://blockchain.info/unspent?active=%s' % address)
 utxo_set = json.loads(resp.text)["unspent_outputs"]
 
 for utxo in utxo_set:
     print("{tx_hash}:{tx_output_n} - {value} Satoshis".format(**utxo))
+
